d7c3c827b1676503186d23b131159d45d7f24840
Sergiodeavilac <sergiodeavilacabral@gmail.com>
2022-09-21 18:10:00 +0200

    All Aproximations and plots OK

--- Milestones/Milestone_1/Module_1.py
+++ Milestones/Milestone_1/Module_1.py
@@ -2,7 +2,10 @@ import numpy as np
 import matplotlib.pyplot as plt
 import Methods as MT
 
-#Initial Values
+###########################################################
+# Initial Values
+###########################################################
+
 r0 = [1,0]
 v0 = [0,1]
 U_0 = r0 + v0
@@ -12,28 +15,37 @@ dt = [0.1, 0.01, 0.001]
 
 U = np.zeros((len(dt), len(U_0), N+1)) #array of zeros
 U_RK4 = np.zeros((len(dt), len(U_0), N+1))#array of zeros
-U_CN = np.zeros((len(U_0), N+1))#array of zeros
+U_CN = np.zeros((len(dt), len(U_0), N+1))#array of zeros
 
 U[:,:,0] = U_0 #Initial conditions    
 U_RK4[:,:,0] = U_0 #Initial conditions
-U_CN[:,0] = U_0
+U_CN[:,:,0] = U_0 #Initial conditions
 
-#Simulations
+###########################################################
+# Simulations
+###########################################################
+
+# Euler aproximation
 for i in range(len(dt)):
-    U[i,:,:] = MT.Euler(U[i,:,:], N, dt[i]) #Euler aproximation
+    U[i,:,:] = MT.Euler(U[i,:,:], N, dt[i]) 
     print('Euler ' + str(dt[i]) + ' done\n')
-
-for i in range(len(dt)): #Rugen-Kutta 4 order aproximation
+    
+# Rugen-Kutta 4 order aproximation
+for i in range(len(dt)): 
     U_RK4[i,:,:] = MT.RK4(U_RK4[i,:,:], N, dt[i])
     print('Rk4 ' + str(dt[i]) + ' done\n' )
 
-U_CN = MT.CN(U_CN, N, dt[1])
-print(str(U_CN))
+# Crank-Nicolson aproximation
+for i in range(len(dt)):
+    U_CN[i,:,:] = MT.CN(U_CN[i,:,:], N, dt[i])
+    print('CN ' + str(dt[i]) + ' done\n')
 
-
+###########################################################
+# Plots
+###########################################################
 
 #Plot 1
-fig, ax = plt.subplots(figsize=(4,4))
+fig, ax = plt.subplots(figsize=(4, 4))
 for i in range(len(dt)):
     ax.plot(U[i,0,:], U[i,1,:], label='Euler Method DeltaT' + str(dt[i]))
 ax.set_xlabel('x')
@@ -54,8 +66,8 @@ plt.show()
 
 #Plot 3
 fig, az = plt.subplots(figsize=(4,4))
-#for i in range(len(dt)):
-az.plot(U_CN[0,:], U_CN[1,:])
+for i in range(len(dt)):
+    az.plot(U_CN[i,0,:], U_CN[i,1,:], label='Crank-Nicolson DeltaT ' + str(dt[i]))
 az.set_xlabel('x')
 az.set_ylabel('y')
 az.set_title('CN')
