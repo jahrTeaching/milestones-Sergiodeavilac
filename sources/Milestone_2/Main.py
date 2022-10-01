79fd56f12e85dd1cab4fcdd90ffd29fe6914323e
Sergiodeavilac <sergiodeavilacabral@gmail.com>
2022-10-01 12:17:08 +0200

    Looking for my own newton integration

--- sources/Milestone_2/Main.py
+++ sources/Milestone_2/Main.py
@@ -10,19 +10,26 @@ r0 = [1.0,0.0]
 v0 = [0.0,1.0]
 U0 = r0 + v0
 N = [1000, 10000, 100000]
- #Number of diferent dt
+
+#Save Plots
+Save = False # TRue Save the plots / False show the plots
+
+#Temporal_Schemes to use
 T_S = [Euler, RK4, CN]
 T_S_plot = ["Euler", "RK4", "CN"]
-Nd = len(T_S)
+
+
 #Initiation of Dictionaries
 U_dic = { }
 t_dic = { }
 
+#Times for simulations
 for i in range(len(N)):
         t_dic[str(i)] = linspace(0,20,N[i])
                   
+#Simulations
 for j in range ( len(T_S_plot) ):
         for x in t_dic: # x is a str and U is creating new keys
                 U_dic[x]= C_P(Kepler, t_dic[x], U0, T_S[j])
                 print(T_S_plot[j] + " calculado \n")
-                Plot_CP(U_dic[x],t_dic[x], Nd, T_S_plot[j]) # x return the value of the key
+                Plot_CP(U_dic[x],t_dic[x], T_S_plot[j], Save) # x return the value of the key
--- sources/Milestone_2/Plots.py
+++ sources/Milestone_2/Plots.py
@@ -2,7 +2,7 @@ import matplotlib.pyplot as plt
 import os
 
 
-def Plot_CP (U,t,Nd, T_S):
+def Plot_CP (U,t, T_S, Save):
     fig, ax = plt.subplots(figsize=(4, 4))
     dt = t[1]-t[0] 
     ax.plot(U[0,:], U[1,:], label= T_S + "dt " + str(dt))
@@ -10,14 +10,20 @@ def Plot_CP (U,t,Nd, T_S):
     ax.set_ylabel('y')
     ax.set_title(T_S)
     ax.legend()
-    plt.show()
     
-    ###Save Plots (Comment to not save) (Read Readme.txt for saving plots)
-    # file = T_S + "_dt_" + str(dt) +".png"
-    # path = os.path.join(os.path.dirname(os.path.abspath("Main_ML2.py")), 'Plots')
+    if not Save:
+        plt.show()
+
+    else:
+        ##Save Plots
+        file = T_S + "_dt_" + str(dt) +".png"
+        path = os.path.join(os.path.dirname(os.path.abspath("Main_ML2.py")), 'Plots')
+        
+        if not os.path.exists(path):
+            os.makedirs(path)
+        
+        plt.savefig(os.path.join(path,file))
+        
     
-    # if not os.path.exists(path):
-    #     os.makedirs(path)
     
-    # plt.savefig(os.path.join(path,file))
     
