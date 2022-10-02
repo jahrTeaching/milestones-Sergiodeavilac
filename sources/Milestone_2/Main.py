c141aff35a22f93a0e5312d55a4499237bcec106
Sergiodeavilac <sergiodeavilacabral@gmail.com>
2022-10-02 17:05:23 +0200

    Milestone 2

--- sources/Milestone_2/Main.py
+++ sources/Milestone_2/Main.py
@@ -1,7 +1,7 @@
 from numpy import zeros, linspace, shape
 from Cauchy_Problem import C_P
 from Physics import Kepler
-from Plots import Plot_CP
+from Plots import Plot_CP, Plot_CP_all
 from Temporal_Schemes import Euler, RK4, CN, Euler_inver
 
 
@@ -12,13 +12,13 @@ U0 = r0 + v0
 N = [1000, 10000, 100000]
 
 #Save Plots
-Save = False # TRue Save the plots / False show the plots
+Save = True # TRue Save the plots / False show the plots
 
 #Temporal_Schemes to use
-# T_S = [Euler, RK4, CN, Euler_inver]
-# T_S_plot = ["Euler", "RK4", "CN", 'Euler_inver']
-T_S = [Euler_inver]
-T_S_plot = ['Euler_inver']
+T_S = [Euler, RK4, CN, Euler_inver]
+T_S_plot = ["Euler", "RK4", "CN", 'Euler_inver']
+# T_S = [Euler]
+# T_S_plot = ['Euler']
 
 #Initiation of Dictionaries
 U_dic = { }
@@ -33,4 +33,5 @@ for j in range ( len(T_S_plot) ):
         for x in t_dic: # x is a str and U is creating new keys
                 U_dic[x]= C_P(Kepler, t_dic[x], U0, T_S[j])
                 print(T_S_plot[j] + " calculado \n")
-                Plot_CP(U_dic[x],t_dic[x], T_S_plot[j], Save) # x return the value of the key
+                # Plot_CP(U_dic[x],t_dic[x], T_S_plot[j], Save) # x return the value of the key
+        Plot_CP_all(U_dic,t_dic, T_S_plot[j], Save) # x return the value of the key
--- sources/Milestone_2/Plots.py
+++ sources/Milestone_2/Plots.py
@@ -2,7 +2,7 @@ import matplotlib.pyplot as plt
 import os
 
 
-def Plot_CP (U,t, T_S, Save):
+def Plot_CP(U,t, T_S, Save):
     fig, ax = plt.subplots(figsize=(4, 4))
     dt = t[1]-t[0] 
     ax.plot(U[0,:], U[1,:], label= T_S + "dt " + str(dt))
@@ -23,6 +23,29 @@ def Plot_CP (U,t, T_S, Save):
             os.makedirs(path)
         
         plt.savefig(os.path.join(path,file))
+        
+def Plot_CP_all(U,t, T_S, Save):
+    fig, ax = plt.subplots(figsize=(4, 4))
+    for i in t:
+        dt = t[i][1]-t[i][0] 
+        ax.plot(U[i][0,:], U[i][1,:], label= T_S + "dt " + str(dt))
+    ax.set_xlabel('x')
+    ax.set_ylabel('y')
+    ax.set_title(T_S)
+    ax.legend()
+    
+    if not Save:
+        plt.show()
+
+    else:
+        ##Save Plots
+        file = T_S +"_all_" "_dt_" + str(dt) +".png"
+        path = os.path.join(os.path.dirname(os.path.abspath("Main.py")), 'Plots')
+        
+        if not os.path.exists(path):
+            os.makedirs(path)
+        
+        plt.savefig(os.path.join(path,file)) 
         
     
     
