2e540b32f3b992422bfa2c0f731b399878ad8fcc
Sergiodeavilac <sergiodeavilacabral@gmail.com>
2022-10-08 15:46:56 +0200

    Update comment

--- sources/ODES/Cauchy_Problem.py
+++ sources/ODES/Cauchy_Problem.py
@@ -3,7 +3,7 @@ from numpy import array, zeros
 def C_P(F, t, U0,T_S):
     
     N = len(t)-1 #Because of the divisons of the time's linspace 
-    U = zeros( (len(U0), N+1 ) )
+    U = zeros( (len(U0), N+1 ) ) #len will give you the number of 'elements' of the first dimension (4)
     U[:,0] = U0
      
     for i in range(N):
