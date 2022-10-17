e1c61ae46d89ebce018b286854b50d36409ed6f2
Sergiodeavilac <sergiodeavilacabral@gmail.com>
2022-10-17 17:02:29 +0200

    LU factorization

--- sources/Milestone_2.py
+++ sources/Milestone_2.py
@@ -9,15 +9,17 @@ from ODES.Temporal_Schemes import Euler, RK4, Crank_Nicolson, Inverse_Euler
 r0 = [1,0]
 v0 = [0,1]
 U0 = r0 + v0
-N = [1000, 10000, 100000]
+# N = [1000, 10000, 100000]
+N = [1000]
 
 #Save Plots
 Save = False # TRue Save the plots / False show the plots
 
 #Temporal_Schemes to use
-T_S = [Euler, RK4, Crank_Nicolson, Inverse_Euler]
-T_S_plot = ["Euler", "RK4", "CN", 'Euler_inver']
-
+# T_S = [Euler, RK4, Crank_Nicolson, Inverse_Euler]
+# T_S_plot = ["Euler", "RK4", "CN", 'Euler_inver']
+T_S = [Inverse_Euler]
+T_S_plot = ['Euler_inver']
 
 #Initiation of Dictionaries
 U_dic = { }
@@ -32,5 +34,5 @@ for j in range ( len(T_S_plot) ):
         for x in t_dic: # x is a str and U is creating new keys
                 U_dic[x]= C_P(Kepler, t_dic[x], U0, T_S[j])
                 print(T_S_plot[j] + " calculado \n")
-                # Plot_CP(U_dic[x],t_dic[x], T_S_plot[j], Save) # x return the value of the key
-        Plot_CP_all(U_dic,t_dic, T_S_plot[j], Save) 
+                Plot_CP(U_dic[x],t_dic[x], T_S_plot[j], Save) # x return the value of the key
+        # Plot_CP_all(U_dic,t_dic, T_S_plot[j], Save) 
--- sources/ODES/Jab_Newt.py
+++ sources/ODES/Jab_Newt.py
@@ -11,7 +11,50 @@ def Jacobiano (F, xp):
         x[j] = dx
         Jab[:,j] = ( F(xp + x ) - F(xp - x) ) /(2*dx)
     return Jab
+
+def LU(A):
+    N = size(A,1)
+    A[1:N,0] = A[1:N,1] / A[0,0]
+    
+    for k in range (1,N):
+        for j in range(k,N):
+            A[k,j] = A[k,j] - dot( A[k, 1:k-1], A[1:k-1, j] )
+            
+    for i in range(k+1,N):
+         A[i,k] =(A[i,k] - dot(A[0:k,k], A[i,0:k])) / (A[k,k])
+
+    return A
+
+def solve_LU(A,b):
+    x = zeros(size(b))
+    y = zeros(size(b))
+    
+   
+    A = LU(A)
+    y[0] = b[0]
+    
+    for i in range(1, size(b) ):
+        y[i] = b[i] - dot( A[i,0:i], y[0:i] )
         
+    x[-1] = y[-1] / A[-1,-1]
+    
+    for i in range(-1, 1, -1):
+        x[i] = ((y[i] - dot(A[i, i+1:size(b)+1], x[i+1:size(b)+1])) / A[i,i])
+    
+    return x
+
+def Inversa(A):
+
+	B = zeros([size(A,1), size(A,1)])
+
+	for i in range(size(A,1)):
+     
+		a = zeros(size(A,1))
+		a[i] = 1
+
+		B[:,i] = solve_LU(A, a)
+
+	return B
         
 def Newton(F,x0):
     N = size(x0)
@@ -28,4 +71,18 @@ def Newton(F,x0):
         x0 = x0 - dx
         eps = norm(dx)
     return x0
+
+# def Newton(F,x0):
+#     N = size(x0)
+#     dx = 2 * x0
+#     it = 0 #Initialization of iterations
+#     itmax = 1000 #max iterations
+#     eps = 1 # initial error
+    
+#     while (eps > 1e-8) and (it <= itmax):
+#         it = it + 1
+#         dx = dot( Inversa(Jacobiano(F, x0)), F(x0))
+#         x0 = x0 - dx
+#         eps = norm(dx)
+#     return x0
 
