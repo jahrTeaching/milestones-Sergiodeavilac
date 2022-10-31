f6b4eec2c7482a802c3a66254d4417f7360d0e85
Sergiodeavilac <sergiodeavilacabral@gmail.com>
2022-10-31 18:50:31 +0100

    Update Milestone 4

--- sources/Milestone_4.py
+++ sources/Milestone_4.py
@@ -1,10 +1,8 @@
 from numpy import array, save, zeros, linspace, shape
 from ODES.Cauchy_Problem import C_P
-from Problems.Physics import Kepler, Linear_Oscilator
+from Problems.Physics import Linear_Oscilator
 from ODES.Temporal_Schemes import Euler, RK4, Crank_Nicolson, Inverse_Euler, LeapFrog
 from Stability_Region.S_R import Stability_Region
-from Error.Richarson import Richarson, Convergence_rate
-from alive_progress import alive_bar #to see the progress of the computations
 import Graf.Plots as plt
 
 #Initial Conditions
@@ -13,7 +11,6 @@ v0 = [0]
 U0 = r0 + v0
 T = 100
 dt = [0.1, 0.01, 0.001]
-# dt = [0.1, 0.15, 0.2]
 
 #Save Plots
 Save = True # True Save the plots / False show the plots
@@ -22,12 +19,7 @@ Save = True # True Save the plots / False show the plots
 T_S = [Euler, RK4, Crank_Nicolson, Inverse_Euler, LeapFrog]
 T_S_plot = ["Euler", "RK4", "CN", 'Euler_inver', "LeapFrog"]
 
-# T_S = [LeapFrog]
-# T_S_plot = ["LeapFrog"]
-
-
 #Initiation of Dictionaries
-Er_dic = { }
 t_dic = { }
 U_dic = { }     
 S_R = { }
@@ -40,12 +32,12 @@ for i in range(len(dt)):
 
 
 # # #Simulations
-# for j in range ( len(T_S_plot) ):
-#         for x in t_dic: # x is a str and U is creating new keys
-#                 U_dic[x]= C_P(Linear_Oscilator, t_dic[x], U0, T_S[j])
-#                 plt.Plot_CP(U_dic[x], t_dic[x], T, dt[int(x)], T_S_plot[j], Save) # x return the value of the key
-#         plt.Plot_CP_all(U_dic,t_dic,T, dt, T_S_plot[j], Save)
-#         print(T_S_plot[j] + " calculado \n")
+for j in range ( len(T_S_plot) ):
+        for x in t_dic: # x is a str and U is creating new keys
+                U_dic[x]= C_P(Linear_Oscilator, t_dic[x], U0, T_S[j])
+                plt.Plot_CP(U_dic[x], t_dic[x], T, dt[int(x)], T_S_plot[j], Save) # x return the value of the key
+        plt.Plot_CP_all(U_dic,t_dic,T, dt, T_S_plot[j], Save)
+        print(T_S_plot[j] + " calculado \n")
 
 #Stability Region
 
